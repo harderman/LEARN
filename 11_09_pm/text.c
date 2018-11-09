437b8ce4bd00e6e1646e59cf1e1ec3c9388bfaaf
handerman <2862551221@qq.com>
2018-11-09 18:01:11 +0800

    大小写字母转换

---
+++ 11_09_pm/text.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int ch;
+	printf("ÇëÊäÈëÒ»¸ö×Ö·û:\n");
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch >= 'a'&&ch <= 'z')
+		{
+			ch = ch - 32;
+			printf("%c\n", ch);
+		}
+		else if (ch >= 'A'&&ch <= 'Z')
+		{
+			printf("%c\n", ch + 32);
+			ch = ch + 32;
+		}
+		else
+		if (ch >= '0'&&ch <= '9')
+			;
+		else
+			;
+	}
+	printf("\n");
+	system("pause");
+}
